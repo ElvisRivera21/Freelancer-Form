265f88c127945bfbab4a073559c3c855c341b681
Elvis Rivera <therealelvisrivera@gmail.com>
2023-11-27 11:33:47 -0600

    git commit -m "Initial commit"

---
+++ freelancer.js
@@ -0,0 +1,27 @@
+//Freelancers Array Containing Workers
+
+const freelancers = [
+    { name: "Alice", occupation: "Writer", startingPrice: 30 },
+    { name: "Bob", occupation: "Teacher", startingPrice: 50 },
+    { name: "Carol", occupation: "Programmer", startingPrice: 70 }
+];
+
+console.log(freelancers);
+
+//Freelancer Function
+
+function renderFreelancers(freelancers) {
+    let html = "";
+
+    freelancers.forEach(freelancer => {
+        const freelancerHTML = `
+            <div class="freelancer">
+                <p>Name: ${freelancer.name}</p>
+                <p>Occupation: ${freelancer.occupation}</p>
+                <p>Starting Price: ${freelancer.startingPrice}</p>
+            </div>
+        `;
+
+        html += freelancerHTML;
+    });
+}
